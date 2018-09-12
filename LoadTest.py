ac9fef337babef2318e5bd2c18b4d435a7eb7925
MoeSoufan <moe.soufan@outlook.com>
2018-09-12 17:02:35 -0600

    Updated loadTestworkflow
    Changed the workflow of how Patient List textbox is searched. Added more protection to failed loading study attempts.

--- LoadTest.py
+++ LoadTest.py
@@ -1,24 +1,40 @@
-from pywinauto import application
+
+        ### Created by: Moatassem Soufan
+        ### Creation Date: September 7, 2018
+
+from pywinauto import application, findbestmatch, findwindows
 import time
 
 
 def load(dialog):
 
-    dialog.Edit.click_input()
-    dialog.Edit.double_click_input()
-    dialog.Edit.set_text('Function-Flow-Perf')
-    dialog.FunctionFlowPerf.double_click_input()
+    study_name = 'SA-3D Demo'
 
-    start = time.time()
-    dialog.LoadingStudyDone.wait('visible',1000)
-    end = time.time()
+    while True:
+        if dialog.Edit.exists() is True and dialog.Toolbar.child_window(title="Patient List").exists() is False:
+            try:
+                # print dialog.Edit.print_identifiers()
+                dialog.Edit.click_input()
+                dialog.Edit.set_text(study_name)
+                dialog.window(title=study_name).double_click_input()
 
-    print "Loading Study Time: %.2f" % (end - start)
+                start = time.time()
+                dialog.LoadingStudyDone.wait('visible', 1000)
+                end = time.time()
+                print "Loading Study Time: %.2f" % (end - start)
+                break
 
-    # while True:
-    #     try:
-    #         loginDialog.Short3D.click()
-    #         break
-    #     except:
+            except findwindows.ElementNotFoundError:
+                try:
+                    dialog.Toolbar.child_window(title="Patient List").click_input()
+                except findwindows.ElementNotFoundError:
+                    print "Study '%s' doesn't exist in database" % study_name
+                    exit()
+
+        elif dialog.Toolbar.child_window(title="Patient List").exists(timeout=1) is True:
+            dialog.Toolbar.child_window(title="Patient List").click_input()
+
+        else:
+            print "Can't find anything"
 
     return
--- main.py
+++ main.py
@@ -1,24 +1,88 @@
-import matplotlib
-import numpy as nm
-import cv2
-import os, sys
-import FeatureDetection
 
-#print sys.argv[0]
-# print sys.path
-projectDir = sys.path[0]
+        ### Created By Moe Soufan
+        ### Creation Date: September 7, 2018
 
-image = "moduleSA3D-icon48.png"
-imageDir = "/imageDatabase/icons/modulebuttons/%s" % image
-template = "Untitled.png"
+        # Main? function for automation efforts
+        # Using pywinauto to detect UI elements on cvi42.exe
+        # Inspect.exe used to identify elements
 
-feature = cv2.imread('C:\Moe-Repo\OpenCV\src\Capture.PNG')
-# cv2.imshow("Test", feature)
-# cv2.waitKey(0)
+# from pywinauto import application
+# from pywinauto import findbestmatch
+# import subprocess
+# import time
+# import admin_test
+# import pywinauto
 
-window = cv2.imread(template)
-# cv2.imshow("cvi42 Window Snapshot", window)
-# cv2.waitKey(0)
+import loadTest
+import initialization
 
-FeatureDetection.compareDetectionMeth(feature, window)
-exit(0)
+# print pywinauto.__version__
+
+# Launch cvi42 if not already running, grab process id. Login method called from within.
+pid, mainDialog = initialization.initialize_session()
+print "Connected to cvi42.exe ProcessID #%s" % pid
+
+# Load test trial
+loadTest.load(mainDialog)
+
+exit()
+
+#
+# LoadTest.load(mainDialog)
+
+# print loginDialog.print_control_identifiers()
+
+# loginDialog.Edit.click_input()
+# loginDialog.Edit.double_click_input()
+# loginDialog.Edit.set_text('Function-Flow-Perf')
+#
+# loginDialog.FunctionFlowPerf.double_click_input()
+#
+# start = time.time()
+# loginDialog.LoadingStudyDone.wait('visible',1000)
+# end = time.time()
+#
+# print "Loading Study Time: %.2f" % (end - start)
+#
+
+
+# app = application.Application(backend="uia").connect(path=r"D:\Moe-Testing\2018-08-15_MontrealReleaseCandidate2\cvi42_5.9.2_(1111)_win_x64\cvi42.exe")
+#
+# mainDialog = app.CircleCardiovascularImaging
+# mainDialog.print_control_identifiers()
+# #
+# mainDialog.NegHRCTAnonymized.static.click_input()
+# print mainDialog.NegHRCTAnonymized.static.texts()[0]
+
+#print_control_identifiers()
+
+
+# mainDialog.MenuItem9.print_control_identifiers()
+# mainDialog.MenuItem9.click_input()
+# test = app.MenuItem9
+# test.Short3D.click_input()
+#("Add Protocol Step->Short 3D")
+# mainDialog.MenuItem9.#.click_input()
+
+
+
+# counter = 0
+# final_counter = 0
+#
+# while True:
+#     try:
+#         # mainDialog.Short3DList.double_click_input()
+#         mainDialog.Desktop1.click_input()
+#         counter += 1
+#
+#         if final_counter == 1:
+#             break
+#
+#     except findbestmatch.MatchError as ME:
+#         mainDialog.List.wheel_mouse_input(wheel_dist=20)
+#         mainDialog.List.wheel_mouse_input(wheel_dist=-25)
+#
+#         if counter <= 2:
+#             mainDialog.AddProtocolStep.click_input()
+#             mainDialog.Short3DMenu.click_input()
+#             final_counter = 1
